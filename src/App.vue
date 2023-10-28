ee901c2d27f59f2d639a829d90a19a93d210e028
Cristhian Sevilla <crissev17@gmail.com>
2023-10-28 12:07:38 -0600

    comenzando con el formulario: Select de monedas

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,37 @@
-<script setup></script>
+<script setup>
+import { ref } from "vue";
 
-<template></template>
+const monedas = ref([
+  { codigo: "USD", texto: "Dolar de Estados Unidos" },
+  { codigo: "MXN", texto: "Peso Mexicano" },
+  { codigo: "EUR", texto: "Euro" },
+  { codigo: "GBP", texto: "Libra Esterlina" },
+]);
+</script>
 
-<style scoped></style>
+<template>
+  <div class="contenedor">
+    <h1 class="titulo">Crypto<span> Check</span></h1>
+    <p>Cotiza Criptomonedas al Instante</p>
+
+    <div class="contenido">
+      <form class="formulario">
+        <div class="campo">
+          <label for="moneda">Elige tu moneda</label>
+          <select id="moneda">
+            <option value="">Selecciona</option>
+            <option v-for="moneda in monedas" :value="moneda.codigo">
+              {{ moneda.texto }}
+            </option>
+          </select>
+        </div>
+      </form>
+    </div>
+  </div>
+</template>
+
+<style>
+p {
+  color: white;
+}
+</style>
